0396be3fcf84af69ea339c76e957da6af3f29ecf
Pedro Henrique Bernardo Marques <jasonvorsesomatado@gmail.com>
2024-10-26 21:16:35 -0300

    Rascunho3.0

--- menu.c
+++ menu.c
@@ -12,7 +12,85 @@
   criado a funcao de cadastrar as informacoes no final*/
 void cadastrofinal(tipolista *l)
 {
-    int opc;
+    int resp;
+    infocontas ContaBancaria;
+    tipoapontador p;
+    tipoapontador aux;
+
+    do
+    {
+        do
+        {
+            tela();
+            telacadastro();
+            gotoxy(7, 24);
+            printf("Digite 0 para sair");
+            gotoxy(50, 7);
+            scanf("%d", &ContaBancaria.codigo_conta);
+            aux = pesquisa(l, ContaBancaria.codigo_conta);
+            if (aux != NULL)
+            {
+                gotoxy(7, 24);
+                printf("Codigo ja cadastrado");
+                getch();
+            }
+        } while (aux != NULL);
+        if (ContaBancaria.codigo_conta != 0)
+        {
+            gotoxy(50, 9);
+            fflush(stdin);
+            fgets(ContaBancaria.banco, 50, stdin);
+            gotoxy(50, 11);
+            fflush(stdin);
+            fgets(ContaBancaria.agencia, 10, stdin);
+            gotoxy(50, 13);
+            fflush(stdin);
+            fgets(ContaBancaria.numero_conta, 20, stdin);
+            gotoxy(50, 15);
+            fflush(stdin);
+            fgets(ContaBancaria.tipo_conta, 20, stdin);
+            gotoxy(50, 17);
+            scanf("%lf", &ContaBancaria.vl_saldo);
+            gotoxy(50, 19);
+            scanf("%lf", &ContaBancaria.vl_limite);
+            gotoxy(50, 21);
+            fflush(stdin);
+            fgets(ContaBancaria.status, 10, stdin);
+            gotoxy(7, 24);
+            // Ira perfuntar se o usuario deseja salvar esses dados ou não
+            printf("Deseja Salvar os dados(1-SIM/2-NAO):");
+            scanf("%d", &resp);
+            // Se ele quiser salvar ele ira salvar os dados no final
+            if (resp == 1)
+            {
+                p = (tipoapontador)malloc(sizeof(tipoitem));
+                p->proximo = NULL;
+                p->conteudo = ContaBancaria;
+                if (l->primeiro == NULL)
+                {
+                    l->primeiro = p;
+                    l->ultimo = p;
+                }
+                else
+                {
+                    l->ultimo->proximo = p;
+                    l->ultimo = p;
+                }
+                gotoxy(07, 24);
+                printf("Cadastro com Sucesso!                 ");
+                getch();
+            }
+            // caso ele não queira cadastrar uma nova Conta ira voltar para a tela de inicio
+            gotoxy(7, 24);
+            printf("Deseja cadastrar uma nova conta Bancaria (1 = sim / 2 = nao)? ");
+            scanf("%d", &resp);
+        }
+    } while (resp == 1);
+}
+/*Autor: Pedro
+  Funcçao que ira posibilitar o cadastro de uma nova conta bancaria no inicio*/
+void cadastrarinicio(tipolista *l)
+{
     int resp;
     infocontas ContaBancaria;
     tipoapontador p;
@@ -23,72 +101,75 @@ void cadastrofinal(tipolista *l)
         {
             tela();
             telacadastro();
+            gotoxy(7, 24);
+            printf("Digite 0 para sair");
             gotoxy(50, 7);
             scanf("%d", &ContaBancaria.codigo_conta);
             aux = pesquisa(l, ContaBancaria.codigo_conta);
-            if (aux == NULL)
+            if (aux != NULL)
             {
                 gotoxy(7, 24);
                 printf("Codigo ja cadastrado");
                 getch();
             }
         } while (aux != NULL);
-            gotoxy(50,9);
+        if (ContaBancaria.codigo_conta != 0)
+        {
+            gotoxy(50, 9);
             fflush(stdin);
-            fgets(ContaBancaria.banco,50,stdin);
-            gotoxy(50,11);
+            fgets(ContaBancaria.banco, 50, stdin);
+            gotoxy(50, 11);
             fflush(stdin);
-            fgets(ContaBancaria.agencia,10,stdin);
-            gotoxy(50,13);
+            fgets(ContaBancaria.agencia, 10, stdin);
+            gotoxy(50, 13);
             fflush(stdin);
-            fgets(ContaBancaria.numero_conta,20,stdin);
-            gotoxy(50,15);
+            fgets(ContaBancaria.numero_conta, 20, stdin);
+            gotoxy(50, 15);
             fflush(stdin);
-            fgets(ContaBancaria.tipo_conta,20,stdin);
-            gotoxy(50,17);
-            scanf("%lf",&ContaBancaria.vl_saldo);
-            gotoxy(50,19);
-            scanf("%lf",&ContaBancaria.vl_limite);
-            gotoxy(50,21);
+            fgets(ContaBancaria.tipo_conta, 20, stdin);
+            gotoxy(50, 17);
+            scanf("%lf", &ContaBancaria.vl_saldo);
+            gotoxy(50, 19);
+            scanf("%lf", &ContaBancaria.vl_limite);
+            gotoxy(50, 21);
             fflush(stdin);
-            fgets(ContaBancaria.status,10,stdin);
-            gotoxy(7,24);
-            //Ira perfuntar se o usuario deseja salvar esses dados ou não
-            printf("Deseja Salvar os dados(1-SIM/2-NAO):");
-            scanf("%d",&opc);
-            //Se ele quiser salvar ele ira salvar os dados no final
-            if(resp==1){
-                p=(tipoapontador)malloc(sizeof(tipoitem));
-                if(p !=NULL){
-            p ->proximo = NULL;
-
-            p ->conteudo = ContaBancaria;
-            if (l->primeiro ==NULL){
-                l->primeiro = p;
-                l->ultimo = p;
-                
-            }else{
-                l->ultimo ->proximo = p;
-                l-> ultimo = p;
-            }
-            gotoxy(7,24);
-            printf("Cadastro com Sucesso!");
+            fgets(ContaBancaria.status, 10, stdin);
             gotoxy(7, 24);
-            printf("Pressione uma tecla para continuar");
-            getch();
+            // Ira perfuntar se o usuario deseja salvar esses dados ou não
+            printf("Deseja Salvar os dados(1-SIM/2-NAO):");
+            scanf("%d", &resp);
+            // Caso ele deseje salvar os dados ira cadastralos no inicio do sistema
+            if (resp == 1)
+            {
+                p = (tipoapontador)malloc(sizeof(tipoitem));
+                p->proximo = NULL;
+                p->conteudo = ContaBancaria;
+                if (l->primeiro == NULL)
+                {
+                    l->primeiro = p;
+                    l->ultimo = p;
+                }
+                else
+                {
+                    p->proximo = l->primeiro;
+                    l->primeiro = p;
+                }
+                gotoxy(07, 24);
+                printf("Cadastro com Sucesso!                 ");
+                getch();
             }
-            }
-            //caso ele não queira cadastrar uma nova Conta ira voltar para a tela de inicio
-        gotoxy(7, 24);
-        printf("Deseja cadastrar um novo funcionario (1 = sim / 2 = nao)? ");
-        scanf("%d",&resp);
-    } while (resp != 2);
+            gotoxy(7, 24);
+            printf("Deseja cadastrar uma nova conta Bancaria (1 = sim / 2 = nao)? ");
+            scanf("%d", &resp);
+        }
+    } while (resp == 1);
 }
 void cadastrar(tipolista *l)
 {
     int opc = 0;
     do
     {
+        tela();
         gotoxy(27, 03);
         printf("CADASTRO DE CONTAS");
         gotoxy(25, 6);
@@ -116,6 +197,9 @@ void cadastrar(tipolista *l)
         {
         case 1:
             cadastrofinal(l);
+            break;
+        case 2:
+            cadastrarinicio(l);
             break;
         }
     } while (opc != 9);
